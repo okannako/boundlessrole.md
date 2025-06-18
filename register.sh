86c9047c30dea1a18026cddb24599d4c43f0bd49
NakoTurk <filmoloji89@gmail.com>
2025-06-18 13:54:48 +0300

    Create register.sh

---
+++ register.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo -e "\033[0;37m"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+echo " #####   ####        ####        ####  ####    ######    ##########  ####    ####  ###########   ####  ####";
+echo " ######  ####       ######       #### ####    ########   ##########  ####    ####  ####   ####   #### ####";
+echo " ####### ####      ###  ###      ########    ####  ####     ####     ####    ####  ####   ####   ########";   
+echo " #### #######     ##########     ########   ####    ####    ####     ####    ####  ###########   ########";
+echo " ####  ######    ############    #### ####   ####  ####     ####     ####    ####  ####  ####    #### ####";  
+echo " ####   #####   ####      ####   ####  ####   ########      ####     ############  ####   ####   ####  ####";
+echo " ####    ####  ####        ####  ####   ####    ####        ####     ############  ####    ####  ####   ####";
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+echo -e '\e[36mTwitter :\e[39m' https://twitter.com/NakoTurk
+echo -e '\e[36mGithub  :\e[39m' https://github.com/okannako
+echo -e '\e[36mYoutube :\e[39m' https://www.youtube.com/@CryptoChainNakoTurk
+echo -e "\e[0m"
+sleep 5
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo -e "\e[1m\e[32m Gerekli Atamaları Yapın.. \e[0m"  && sleep 2
+
+read -p "Alchemy Rpc Girin: " ALCHEMYRPC
+read -p "Cüzdan Private Key Girİn: " PRIVKEY
+
+echo -e "\e[1m\e[32m Güncellemeler ve Bütün Gereksinimler Yükleniyor. Bitene kadar Bekleyin.. \e[0m"  && sleep 2
+
+sudo apt update && sudo apt install make gcc ocl-icd-opencl-dev nano tmux ocl-icd-libopencl1 libleveldb-dev libopencl-clang-dev libgomp1 curl git tar wget build-essential jq -y && sudo apt install wget -y && cd $HOME
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+git clone https://github.com/boundless-xyz/boundless
+cd boundless
+curl -L https://risczero.com/install | bash
+source ~/.bashrc
+rzup install
+cargo install --git https://github.com/risc0/risc0 bento-client --bin bento_cli
+export PATH="$HOME/.cargo/bin:$PATH"
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+cargo install --locked boundless-cli
+cat <<EOF >> .env.base
+# Base contract addresses
+export VERIFIER_ADDRESS=0x0b144e07a0826182b6b59788c34b32bfa86fb711
+export BOUNDLESS_MARKET_ADDRESS=0x26759dbB201aFbA361Bec78E097Aa3942B0b4AB8
+export SET_VERIFIER_ADDRESS=0x8C5a8b5cC272Fe2b74D18843CF9C3aCBc952a760
+
+# Public order stream URL
+export ORDER_STREAM_URL="https://base-mainnet.beboundless.xyz"
+export ETH_RPC_URL="$ALCHEMYRPC"
+export PRIVATE_KEY="$PRIVKEY"
+EOF
+source .env.base
+
+echo -e "\e[1m\e[32m Yükleme İşlemleri Tamamlandı. Kılavuz Üzerindeki Diğer Adımlara Geçebilirsiniz. \e[0m"  && sleep 2
