72a1f8b685dbe8a144b3299e6028e7800403e443
NakoTurk <filmoloji89@gmail.com>
2025-06-18 15:41:22 +0300

    Update register.sh

--- register.sh
+++ register.sh
@@ -21,8 +21,9 @@ echo -e "\e[1m\e[32m Gerekli Atamaları Yapın.. \e[0m"  && sleep 2
 
 read -p "Alchemy Rpc Girin: " ALCHEMYRPC
 read -p "Cüzdan Private Key Girin: " PRIVKEY
-export ALCHEMYRPC
-export PRIVKEY
+echo "export ALCHEMYRPC=\"$ALCHEMYRPC\"" >> ~/.bashrc
+echo "export PRIVKEY=\"$PRIVKEY\"" >> ~/.bashrc
+source ~/.bashrc
 
 echo -e "\e[1m\e[32m Güncellemeler ve Bütün Gereksinimler Yükleniyor. Bitene kadar Bekleyin.. \e[0m"  && sleep 2
 
@@ -54,5 +55,6 @@ export ETH_RPC_URL="$ALCHEMYRPC"
 export PRIVATE_KEY="$PRIVKEY"
 EOF
 source .env.base
+source ~/.bashrc
 
 echo -e "\e[1m\e[32m Yükleme işlemleri tamamlandı. Kılavuz üzerindeki diğer adımlara geçebilirsiniz. \e[0m"  && sleep 2
